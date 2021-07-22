ccf25f601a93ac14d0348db32ff844850009bd61
Rebecca Williams <rebecca@informedk12.com>
2021-07-22 16:55:44 -0400

    Finish layout and routes.

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,10 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
-  describe "GET /new" do
+
+  describe "should get new" do
     it "returns http success" do
-      get "/users/new"
-      expect(response).to have_http_status(:success)
+      get signup_path
+      # expect(response).to have_http_status(:success)
+      assert_response :success
+
     end
   end
 
