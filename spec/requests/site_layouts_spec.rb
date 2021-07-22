f579f140715a2ff533226d43ca37e68a494ccb06
Rebecca Williams <rebecca@informedk12.com>
2021-07-22 15:56:09 -0400

    Update test

--- spec/requests/site_layouts_spec.rb
+++ spec/requests/site_layouts_spec.rb
@@ -3,8 +3,15 @@ require 'rails_helper'
 RSpec.describe "SiteLayouts", type: :request do
   describe "GET /site_layouts" do
     it "works! (now write some real specs)" do
-      get site_layouts_path
-      expect(response).to have_http_status(200)
+      get root_path
+      assert_template 'static_pages/home'
+      assert_select "a[href=?]", root_path, count: 2
+      assert_select "a[href=?]", help_path
+      assert_select "a[href=?]", about_path
+      assert_select "a[href=?]", contact_path
+      get contact_path
+      assert_select "title", full_title("Contact")
+
     end
   end
 end
